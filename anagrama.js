434e6d4bbf7d81d3fdcd0f92297d8dde939fca61
ThiagoSantos-devthb <thibbatista@gmail.com>
2022-01-27 18:26:16 -0300

    Comentado

--- anagrama.js
+++ anagrama.js
@@ -1,38 +1,38 @@
 
-var anagrama = []
-var notAnagrama = []
+var anagram = []
+var notAnagram = []
 var arrayOrden = []
 
-var listString = ["thiago", "amor", "roma", "escada ", "mora", "gato", "cachorro", "omar", "ator", "rato"]
+var listString = ["thiago", "amor", "roma", "escada ", "mora", "gato", "cachorro", "omar", "ramo"] // Entrada
 
 arrayOrden = arraySort(listString)
-anagrama =  filterAnagram(arrayOrden)
-notAnagrama = DifferenceBetweenArrays(listString, anagrama, notAnagrama)
+anagram = filterAnagram(arrayOrden)
+notAnagram = DifferenceBetweenArrays(listString, anagram, notAnagram)
 
-console.log(arrayOrden )
-console.log(anagrama)
-console.log(notAnagrama)
-
-
-
-console.log("São anagramas: " + anagrama )
-console.log("Não são anagramas: " + notAnagrama)
+// console.log(arrayOrden )
+// console.log(anagrama)
+// console.log(notAnagrama)
 
 
+
+console.log("São anagramas: " + anagram)
+console.log("Não são anagramas: " + notAnagram)
+
+// separa a string por letras e reordena, gerando um array de todas as strings por ordem alfabetica
 function arraySort(arr) {
     let aux = []
     for (let i = 0; i < arr.length; i++) {
         let strings = arr[i].split("").sort()
         let word = strings.join("")
         aux.push(word)
-      
+
     }
-   // console.log(aux)
-    return(aux)
-   
+    // console.log(aux)
+    return (aux)
+
 }
 
-
+// filtra o array ordenado por ordem alfabetica e compara cada elemento e retorna os que possuem igualdade
 function filterAnagram(arr) {
     let aux = []
     let result = []
@@ -49,10 +49,10 @@ function filterAnagram(arr) {
     result = aux.filter(function (elem, pos, self) {
         return self.indexOf(elem) == pos;
     })
-    return(result)
+    return (result)
 }
 
-
+// compara o array de anagramas com o array de entrada , a diferença é o array de "não anagramas" e cria um novo array de "não anagramas"
 function DifferenceBetweenArrays(arr1, arr2, result) {
 
     arr1.filter(function (element) {
@@ -60,9 +60,5 @@ function DifferenceBetweenArrays(arr1, arr2, result) {
             result.push(element)
         }
     });
-        return(result)
-}
-
-function grupoAnagrama(arr){
-    arraySort(arr)
+    return (result)
 }
